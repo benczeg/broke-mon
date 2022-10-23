84ed5b681f97e25b70f4ae287dcd45ca7829a386
benczeg <benczegabor24@gmail.com>
2022-10-23 16:25:43 +0200

    spring boot basic project stuffs added

---
+++ src/test/java/com/brokemon/BrokeMonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokeMonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
